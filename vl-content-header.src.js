8c3ae0990d1e88e41b6bbeb1eb08841c14f740a0
Pascal Riquier <riquipa@riquipa.local>
2019-12-19 09:39:07 +0100

    first commit

---
+++ vl-content-header.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlContentHeader
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-content-header/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-content-header/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-content-header.html|Demo}
+ * 
+ */
+export class VlContentHeader extends VlElement(HTMLElement) {}
+
+define('vl-content-header', VlContentHeader);
